abecf60b85bb91e049076f742d3b26ad0364449f
alakbar97 <alakbarsa@code.edu.az>
2019-01-16 17:07:51 +0400

    responsive done

--- home.js
+++ home.js
@@ -7,4 +7,12 @@ $(document).ready(function () {
       var Text1 = $(this).text();
       $(".flg").text(Text1);
    });
+   $(window).scroll(function () {
+      if ($(window).scrollTop()<150) {
+          $(".uphead").css({"display":"none"})
+      }
+      else{
+          $(".uphead").css({"display":"block"})
+      }
+  })
 })
