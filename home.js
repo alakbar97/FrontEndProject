a896ae952b3227a78ee9801b06b04c2e5ec7fbd4
alakbar97 <alakbarsa@code.edu.az>
2019-01-17 17:39:57 +0400

    reng changer added

--- home.js
+++ home.js
@@ -1,34 +1,42 @@
 $(document).ready(function () {
-   $(".money").click(function () {
-      var Text1 = $(this).text();
-      $(".mny").text(Text1);
-   });
-   $(".flag").click(function () {
-      var Text1 = $(this).text();
-      $(".flg").text(Text1);
-   });
-   $(window).scroll(function () {
-      if ($(window).scrollTop()<150) {
-          $(".uphead").css({"display":"none"})
-      }
-      else{
-          $(".uphead").css({"display":"block"})
-      }
-  })
+    $(".money").click(function () {
+        var Text1 = $(this).text();
+        $(".mny").text(Text1);
+    });
+    $(".flag").click(function () {
+        var Text1 = $(this).text();
+        $(".flg").text(Text1);
+    });
+    $(window).scroll(function () {
+        if ($(window).scrollTop() < 150) {
+            $(".uphead").css({
+                "display": "none"
+            })
+        } else {
+            $(".uphead").css({
+                "display": "block"
+            })
+        }
+    })
+    $(".lefter").click(function() {
+        $(".reng").toggleClass("sidenav");
+        $(".lefter").toggleClass("colors");
+        
+    })
 })
 $('.owl-carousel').owlCarousel({
-   loop:true,
-   margin:10,
-   nav:true,
-   responsive:{
-       0:{
-           items:1
-       },
-       600:{
-           items:2
-       },
-       1000:{
-           items:5
-       }
-   }
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 5
+        }
+    }
 })
