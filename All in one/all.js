635a5d1b75414985eaf64137395a1b813db2635e
alakbar97 <alakbarsa@code.edu.az>
2019-01-22 14:24:01 +0400

    2 col is done

--- All in one/all.js
+++ All in one/all.js
@@ -91,4 +91,20 @@ $(document).ready(function () {
         var Text3=$(this).text();
         $(".showing").text(Text3);
     })
+    $('.owl-carousel').owlCarousel({
+        loop:false,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
 })
