f8e8dc85a09d7745a67a86354758e7d16d26f8f1
alakbar97 <alakbarsa@code.edu.az>
2019-01-24 00:54:05 +0400

    review tab ready

--- All in one/all.js
+++ All in one/all.js
@@ -94,7 +94,7 @@ $(document).ready(function () {
     $('.owl-carousel').owlCarousel({
         loop:false,
         margin:10,
-        nav:true,
+        nav:false,
         responsive:{
             0:{
                 items:1
@@ -108,3 +108,19 @@ $(document).ready(function () {
         }
     })
 })
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
