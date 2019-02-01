fb42e4d3e5147efacdc85479aac88136c8a1a964
alakbar97 <alakbarsa@code.edu.az>
2019-02-01 15:05:11 +0400

    acc removed

--- Thriller page/thriller.js
+++ Thriller page/thriller.js
@@ -95,20 +95,6 @@ $(document).ready(function () {
         $(".showing").text(Text3);
     })
 })
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
-}
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
 output.innerHTML = slider.value;
