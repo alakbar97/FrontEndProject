784e89887fe16d5f6518eb63b10525f7e0709061
alakbar97 <alakbarsa@code.edu.az>
2019-02-02 01:12:54 +0400

    js added

--- Thriller page/thriller.js
+++ Thriller page/thriller.js
@@ -109,3 +109,26 @@ outputer.innerHTML = slide.value;
 slide.oninput = function() {
   outputer.innerHTML = this.value;
 }
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
+
+
+  var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var dropdownContent = this.nextElementSibling;
+  if (dropdownContent.style.display === "block") {
+  dropdownContent.style.display = "none";
+  } else {
+  dropdownContent.style.display = "block";
+  }
+}
+  )}
--- checkout/all.js
+++ checkout/all.js
@@ -88,3 +88,26 @@ $(document).ready(function () {
         $(this).css({"border-bottom":"1px solid red"})
     })
 })
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
+
+
+  var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var dropdownContent = this.nextElementSibling;
+  if (dropdownContent.style.display === "block") {
+  dropdownContent.style.display = "none";
+  } else {
+  dropdownContent.style.display = "block";
+  }
+}
+  )}
